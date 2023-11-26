8b974cf87f2a0a168861c61c4d38504d06ca40f3
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2023-11-26 03:41:36 +0300

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector("#card-template");
 
-// @todo: Функция создания карточки
+// DOM узлы
 
-// @todo: Функция удаления карточки
+const placesContainer = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+// Функция создания карточки
+
+function createCard(card, handleDeleteCard) {
+  const element = cardTemplate.content.querySelector('.card').cloneNode(true);
+  const image = element.querySelector(".card__image");
+  const title = element.querySelector(".card__title");
+  const deleteButton = element.querySelector(".card__delete-button");
+
+  image.src = card.link;
+  image.alt = card.name;
+  title.textContent = card.name;
+  deleteButton.addEventListener('click', function () {
+    handleDeleteCard(element);
+  });
+
+  return element;
+}
+
+// Функция удаления карточки
+
+function deleteCard(element) {
+  element.remove();
+}
+
+// Вывести карточки на страницу
+
+function renderCards(cards) {
+  cards.forEach(card => {
+    const element = createCard(card, deleteCard);
+    placesContainer.appendChild(element);
+  });
+}
+
+renderCards(initialCards);
